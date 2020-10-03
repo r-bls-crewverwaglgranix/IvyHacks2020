a64535904b2ea96020448a7128add0976ce3c2a2
David Chen <chendavid2012@gmail.com>
2020-10-03 12:51:37 -0400

    photon setup stuff

---
+++ Assets/Scripts/Scenes/Launcher.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Com.Bcvggn.IvyHacks2020
+{
+	public class Launcher : MonoBehaviour
+	{
+		#region Private Serializable Fields
+
+		#endregion
+
+
+		#region Private Fields
+
+		/// <summary>
+	    /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
+	    /// </summary>
+		string gameVersion = "1";
+
+		#endregion
+
+
+		#region MonoBehaviour CallBacks
+		
+		void Awake() 
+		{
+	    	PhotonNetwork.AutomaticallySyncScene = true;
+		}
+
+	    // Start is called before the first frame update
+	    void Start() {}
+
+	    #endregion
+
+
+	    #region Public Methods
+	    public void Connect()
+	    {
+	    	if (PhotonNetwork.IsConnected)
+	    	{
+	    		PhotonNetwork.JoinRandomRoom();
+	    	}	
+	    	else
+	    	{
+	    		PhotonNetwork.ConnectUsingSettings();
+	    		PhotonNetwork.GameVersion = gameVersion;
+	    	}
+	    }
+	    #endregion
+	}
+}
---
+++ Assets/Scripts/Scenes/PlayerNameInputField.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.Bcgvvn.IvyHacks2020 
+{
+	[RequireComponent(typeof(InputField))]
+
+	public class PlayerNameInputField : MonoBehaviour
+	{
+		#region Private Constants
+
+		const string playernamePrefKey = "PlyaerName";
+
+		#endregion
+
+
+		#region MonoBehaviour CallBacks
+
+	    // Start is called before the first frame update
+	    void Start()
+	    {
+	    	string defaultname = string.Empty;
+	    	InputField _inputField = this.GetComponent<InputField>();
+	    	if (_inputField!=null)
+	    	{
+	    		if (PlayerPrefs.HasKey(playernamePrefKey))
+	    		{
+	    			defaultName = PlayerPrefs.GetString(playernamePrefKey);
+	    			_inputField.text = defaultName;
+	    		}
+	    	}
+
+	    	PhotonNetwork.NickName = defaultName;
+	        
+	    }
+	    #endregion
+
+
+	    #region Public Methods
+
+	    public void SetPlayerName(string value)
+	    {
+	    	if (string.IsNullOrEmpty(value))
+	    	{
+	    		Debug.LogError("Player Name is null or empty");
+	    		return;
+	    	}
+	    	PhotonNetwork.NickName = value;
+
+	    	PlayerPrefs.SetString(playernamePrefKey, value);
+	    }
+
+	    #endregion
+
+	}
+}
